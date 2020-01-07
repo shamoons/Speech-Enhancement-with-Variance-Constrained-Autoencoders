e2eb20a53d150ea70a1ff4c27630093de1657ab4
Shamoon Siddiqui <shamoon@develop.io>
2020-01-07 09:09:39 -0500

    Removed packages

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=['sevcae'],
+    # packages=['sevcae'],
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
