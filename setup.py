4252dda687e3b3bb3715aed82318aca44429f8c6
Shamoon Siddiqui <shamoon@develop.io>
2020-01-07 09:07:44 -0500

    Changed name

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ here = path.abspath(path.dirname(__file__))
 # Get the long description from the README file
 
 setup(
-    name='ahoproc_tools',
+    name='sevcae',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
